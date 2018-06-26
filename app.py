cdeb8cb0cb74016a6c37b34c7840e86ce383549c
Fabio Marigo <39345385+fabiomarigo7@users.noreply.github.com>
2018-06-26 23:00:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import os
 from flask import Flask, request, redirect, url_for, send_from_directory, render_template
-#import cv2
+import cv2
 import sys
-#import datetime
-#import traceback
-#import numpy as np
-#from CropImagesFunctions import cif_step1, cif_step2, cif_step3, cif_isoutlier
+import numpy as np
+from CropImagesFunctions import cif_step1, cif_step2, cif_step3, cif_isoutlier
 
 app = Flask(__name__,static_folder=None)
 
@@ -19,21 +17,21 @@ def hello_world():
 @app.route('/upload', methods=['POST'])
 def upload_file():
     #read image file string data
-    file = request.files['image']
-    #filestr = request.files['image'].read()
+    #file = request.files['image']
+    filestr = request.files['image'].read()
     #convert string data to numpy array
-    #npimg = np.fromstring(filestr, np.uint8)
+    npimg = np.fromstring(filestr, np.uint8)
     # convert numpy array to image
-    #img = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
-    #step1 = cif_step1(img)
-    #step2 = cif_step2(step1)
-    #step3 = cif_step3(step2)
+    img = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
+    step1 = cif_step1(img)
+    step2 = cif_step2(step1)
+    step3 = cif_step3(step2)
     
     filename='foto.jpg'
     f = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     #print f, UPLOAD_FOLDER
-    #cv2.imwrite(f, step3)
-    file.save(f)
+    cv2.imwrite(f, step3)
+    #file.save(f)
     return redirect(url_for('uploaded_file', filename=filename))
     
 @app.route('/show/<filename>')
